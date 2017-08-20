f63637ad4e99d415278bebf051af8a42a09d14f3
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2017-08-21 02:51:14 +0530

    LanguageTranslation_online.py

---
+++ 2. Language_Translator/LanguageTranslation_online.py
@@ -0,0 +1,8 @@
+#using baidu, Google, bing translating the language
+#install translation : pip install translation
+
+from translation import baidu, google, bing
+#<translator_API> ('text',dst='language_code you wanna convert it to')
+print(baidu('hello,nice to meet you.', dst = 'fr'))
+print(bing('hello,nice to meet you.', dst = 'hi'))
+print(bing('हैलो!', dst = 'en'))
