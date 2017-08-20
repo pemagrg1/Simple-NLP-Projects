999e3ef1049aaa1a408c3c43e3498ad636c84f18
pemagrg1 <pema.grg1@gmail.com>
2017-08-21 02:22:27 +0530

    adding print text

--- 1. NER/simple_NER.py
+++ 1. NER/simple_NER.py
@@ -1,12 +1,13 @@
 import nltk
-from nltk.corpus import state_union
-from nltk.tokenize import PunktSentenceTokenizer
 
 text="This is the beginning of the NER TEXT where it identifies names like John,Steve and organization like Samsung and Apple."
 sentences = nltk.sent_tokenize(text)
 for sentence in sentences:
-    words = nltk.word_tokenize(i)
-    tagged = nltk.pos_tag(words)
-    namedEnt = nltk.ne_chunk(tagged, binary=True)
-    namedEnt.draw()
+    tokens = nltk.word_tokenize(sentence)
+    print("TOKENS: \n", tokens,"\n")
+    token_pos = nltk.pos_tag(tokens)
+    print ("TOKEN POS: \n", token_pos,"\n")
+    NER = nltk.ne_chunk(token_pos)
+    print ("NER:\n:",NER,"\n")
+    #namedEnt.draw() #for visualization 
     
