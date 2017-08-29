ba47fa675a7ce5d1cc24db4130e7b3e0055fc422
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2017-08-30 02:02:38 +0530

    Update simple_NER.py

--- 1. Named_Entity_Recognition/simple_NER.py
+++ 1. Named_Entity_Recognition/simple_NER.py
@@ -1,3 +1,7 @@
+'''
+Author: Pema Gurung
+Date: 20-7-17
+'''
 import nltk
 
 text="This is the beginning of the NER TEXT where it identifies names like John,Steve and organization like Samsung and Apple."
