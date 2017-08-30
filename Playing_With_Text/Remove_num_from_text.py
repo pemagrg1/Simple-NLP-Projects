9e22d698b265876084456992f5d70077932516c1
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2017-08-31 00:09:35 +0530

    Update Remove_num_from_text.py

--- Playing_With_Text/Remove_num_from_text.py
+++ Playing_With_Text/Remove_num_from_text.py
@@ -2,4 +2,37 @@
 Author: Pema Gurung
 Date: 30-8-17
 '''
-#ways to remove text
+#ways to remove text, i'll show you an example using various methods
+
+#isdigit()
+def isdigit():
+    without_join=[i for i in str if not i.isdigit()]
+    with_join=''.join([i for i in str if not i.isdigit()])
+    print ("isdigit():without join:",without_join)
+    print ("isdigit():with join:",with_join)
+    print ("###############################")
+    return
+
+#isnumeric()
+def isnumeric():
+    without_join=[i for i in unicode_str if not i.isnumeric()]
+    with_join=''.join([i for i in unicode_str if not i.isnumeric()])
+    print ("isnumeric():without join:",without_join)
+    print ("isnumeric():with join:",with_join)
+    print ("###############################")
+    return
+
+#regex
+def regex():
+    import re
+    reg = re.sub(r'\d+', '', str)
+    print ("regex:",reg)
+    print ("###############################")
+    return
+
+
+str="123abd4d56"
+unicode_str=u"123abd4d56"
+isdigit()
+isnumeric()
+regex()
