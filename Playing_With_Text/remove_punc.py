8b2ac67b89e16f11f56902e94601ceb1bf270421
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2017-09-05 01:01:28 +0530

    Update remove_punc.py

--- Playing_With_Text/remove_punc.py
+++ Playing_With_Text/remove_punc.py
@@ -2,3 +2,22 @@
 Author: Pema Gurung
 Date: 5-9-17
 '''
+text="""Go for pearl strings or delicate multi-layer pieces when you attend a "summer wedding".While talking about "head-turning earrings", they should be equally sparkly,  but somewhat lighter in design to avoid a melodramatic look."""
+
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+clean_text = ""
+for word in text:
+   if word not in punctuations:
+       clean_text = clean_text + word
+
+# display the unpunctuated string
+print(clean_text)
+
+#########USING NLTK REGEXPTOKENIZER#######
+from nltk.tokenize import RegexpTokenizer
+
+tokenizer = RegexpTokenizer(r'\w+')
+final_text=tokenizer.tokenize(text)
+print (final_text)
+
