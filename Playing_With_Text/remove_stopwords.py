7dbea9a3127a8f4021390b5f42ef6397f0b97ab9
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2017-09-06 12:20:38 +0530

    Update remove_stopwords.py

--- Playing_With_Text/remove_stopwords.py
+++ Playing_With_Text/remove_stopwords.py
@@ -4,7 +4,6 @@ Date: 6-9-17
 '''
 
 #removing stopwords using NLTK
-
 import nltk
 from nltk.corpus import stopwords
 
@@ -12,4 +11,11 @@ text= "Here I am with a simple text. This, is an example to remove stopwords."
 stop_words=set(stopwords.words('english'))
 word_tokens=nltk.word_tokenize(text)
 final_text=[i for i in word_tokens if i not in stop_words]
-print (final_text)
+print ("USING NLTK:",final_text)
+
+###other way
+text= "Here I am with a simple text. This, is an example to remove stopwords."
+stopwords=['is','am','a','an','to'] #define your stopwords list
+word_tokens=nltk.word_tokenize(text)
+final_text=[i for i in word_tokens if i not in stopwords]
+print ("USING defined STOPWORDS:",final_text)
