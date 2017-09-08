fc77cee6f603a382b834219237d50fcd02166fd6
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2017-09-08 19:06:51 +0530

    Update remove_punc.py

--- Playing_With_Text/remove_punc.py
+++ Playing_With_Text/remove_punc.py
@@ -6,18 +6,30 @@ text="""Go for pearl strings or delicate multi-layer pieces when you attend a "s
 
 punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
-clean_text = ""
-for word in text:
-   if word not in punctuations:
-       clean_text = clean_text + word
+no_punct = ""
+for char in text:
+   if char not in punctuations:
+       no_punct = no_punct + char
 
 # display the unpunctuated string
-print(clean_text)
+print(no_punct)
 
 #########USING NLTK REGEXPTOKENIZER#######
 from nltk.tokenize import RegexpTokenizer
+text="""Go for pearl strings or delicate multi-layer pieces when you attend a "summer wedding".While talking about "head-turning earrings", they should be equally sparkly,  but somewhat lighter in design to avoid a melodramatic look."""
 
 tokenizer = RegexpTokenizer(r'\w+')
 final_text=tokenizer.tokenize(text)
 print (final_text)
+
+#############Using replace##
+text="""Go for pearl strings or delicate multi-layer pieces when you attend a "summer wedding".While talking about "head-turning earrings", they should be equally sparkly,  but somewhat lighter in design to avoid a melodramatic look."""
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+no_punct = text
+for char in text:
+   if char in punctuations:
+       no_punct = no_punct.replace(char," ")
+# display the unpunctuated string
+print(no_punct)
 
