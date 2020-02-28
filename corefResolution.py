fafb5046a626806315d99f2f3618f9a5f9094cdc
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2020-02-28 14:33:57 +0545

    Create corefResolution.py

---
+++ corefResolution.py
@@ -0,0 +1,19 @@
+"""
+INSTALLATIONS: 
+  pip install neuralcoref --no-binary neuralcoref
+  pip install -U spacy
+  python -m spacy download en
+"""
+
+import neuralcoref
+import spacy
+
+nlp = spacy.load('en')
+# Add neural coref to SpaCy's pipe
+neuralcoref.add_to_pipe(nlp)
+
+# You're done. You can now use NeuralCoref as you usually manipulate a SpaCy document annotations.
+doc = nlp(u'My sister has a dog. She loves him.')
+
+print(doc._.has_coref)
+print(doc._.coref_clusters)
