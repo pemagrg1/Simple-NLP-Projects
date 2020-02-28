466e29ca71f05d4b6b457a75c59b9e65ce1ca9b2
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2020-02-28 17:11:09 +0545

    Update corefResolution.py

--- corefResolution.py
+++ corefResolution.py
@@ -17,3 +17,9 @@ doc = nlp(u'My sister has a dog. She loves him.')
 
 print(doc._.has_coref)
 print(doc._.coref_clusters)
+
+"""
+result:
+True
+[My sister: [My sister, She], a dog: [a dog, him]]
+"""
