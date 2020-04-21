956f4ba477ac64e8c0f3c655638280b1156138df
pemagrg1 <pema.grg1@gmail.com>
2020-04-21 11:07:10 +0545

    training the classifier and saving it into pickle

--- 08. Multi-class_text_classification/train_classifier.py
+++ 08. Multi-class_text_classification/train_classifier.py
@@ -4,66 +4,47 @@
     classes in the dataset: ['tech', 'business', 'sport', 'entertainment', 'politics']
 
 """
-from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
-from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import pickle
-from sklearn import linear_model
-Project_path = "/media/ekbana/ekbana500/MY GITHUB/Machine_Learning-NLP_programs"
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+import pickle
+import joblib
 
 data = pd.read_csv('https://storage.googleapis.com/dataset-uploader/bbc/bbc-text.csv')
 print (data.category.unique())
-vectorizer = TfidfVectorizer(sublinear_tf=True, encoding='utf-8',
-                             decode_error='ignore')
+print(data.head(2))
+
+def save_pkl_joblib(model_path,vectorizer_path):
+    joblib.dump(model, model_path+"model.sav")
+    joblib.dump(vectorizer_model, vectorizer_path+"vectorizer.sav")
+    print ("====done saving into pickle using Joblib!====")
 
 
-def train_test(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-    print("==train test split done==")
-    model = linear_model.SGDClassifier(learning_rate='constant',
-                                       eta0=0.1, shuffle=False,
-                                       n_iter=1)
-
-    print("==fitting the model===")
-    model.fit(X_train.A, y_train)
-
-    print("==fit done\=")
-    ypred = model.predict(X_test.A)
-
-    print("==calculating score==")
-    score = accuracy_score(y_test, ypred)
-    print(score)
+def save_pkl_pickle(model_path,vectorizer_path):
+    pickle.dump(model, open(model_path+"model.pickle", 'wb'))
+    pickle.dump(vectorizer_model, open(vectorizer_path+"vectorizer.pickle", "wb"))
+    print ("====done saving into pickle using Pickle!====")
 
 
-def train_bpsd(df, vectorizer):
-    tfidf = vectorizer.fit(df["text"].values.astype('U'))
+def train_test_model(X, y):
+    #Vectorizer
+    vectorizer_model = TfidfVectorizer(stop_words='english')
+    X = vectorizer_model.fit_transform(X.values.astype('U'))
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    X = vectorizer.fit_transform(df["text"].values.astype('U'))
-    y = df['category']
-    # train_test(X, y)
-    # model = svm.LinearSVC()
-    model = linear_model.SGDClassifier(learning_rate='constant',
-                                       eta0=0.1, shuffle=False,
-                                       n_iter=1)
+    # MODEL Training
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    print(model.score(X_test, y_test))
 
-    print("==fitting the model===")
-    model.fit(X.A, y)
+    return model, vectorizer_model
 
-    print("==fit done\=")
-    return model, tfidf
+Project_path = "Machine_Learning-NLP_programs"
+model_path = Project_path + "/08. Multi-class_text_classification/models/"
+vectorizer_path = Project_path + "/08. Multi-class_text_classification/models/"
 
-
-
-model_path = Project_path + "/08. Multi-class_text_classification/models/model.pickle"
-vectorizer_path = Project_path + "/08. Multi-class_text_classification/models/vectorizer.pickle"
-import time
-st = time.time()
-model, vectorizer_model = train_bpsd(data, vectorizer)
-print (time.time()-st)
-pickle.dump(model, open(model_path, 'wb'))
-pickle.dump(vectorizer_model, open(vectorizer_path, "wb"))
-
+model, vectorizer_model = train_test_model(data["text"], data["category"])
+save_pkl_pickle(model_path,vectorizer_path)
+save_pkl_joblib(model_path,vectorizer_path)
