ba34ba38d99c0d21ab2560e473fe058eeba4a335
pemagrg1 <pema.grg1@gmail.com>
2020-05-01 19:04:52 +0545

    sentiment classificaiotn

---
+++ 07. Text_Classification/sentiment_classification/sentiment_classifcation.py
@@ -0,0 +1,30 @@
+import re
+from textblob import TextBlob
+
+
+class SentimentClassification():
+    def __init__(self):
+        pass
+
+    def clean_tweet(self, tweet):
+        '''
+        Utility function to clean tweet text by removing links, special characters
+        using simple regex statements.
+        '''
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])| (\w+:\ / \ / \S+)", " ", tweet).split())
+
+    def nltk_sentiment(self):
+        pass
+
+    def textblob_sentiment(self,tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+        if analysis.sentiment.polarity > 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
+
+    def logistic_regression(self):
+        pass
+
